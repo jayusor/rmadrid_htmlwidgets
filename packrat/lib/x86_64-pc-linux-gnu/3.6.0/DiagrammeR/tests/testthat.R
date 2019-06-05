0d936bfe1701c3a26e9f81739267e024385b89f3
Jorge Ayuso <jayusor@gmail.com>
2019-06-05 21:45:57 +0200

    Remove ignored files

--- packrat/lib/x86_64-pc-linux-gnu/3.6.0/DiagrammeR/tests/testthat.R
+++
@@ -1,5 +0,0 @@
-library(testthat)
-library(DiagrammeR)
-
-suppressWarnings(RNGversion("3.5.0"))
-test_check("DiagrammeR")
--- packrat/lib/x86_64-pc-linux-gnu/3.6.0/readr/tests/testthat/test-source.R
+++
@@ -1,5 +0,0 @@
-context("test-source.R")
-
-test_that("standardise_path works", {
-  expect_error(standardise_path("https://foo/bar.bz2"), "compressed files is not supported")
-})
